9c191c810ae097e1b5d6cb8e4b005229064241dd
Sylvain Malnuit <sylvain.malnuit@lyra-network.com>
2019-10-07 13:27:10 +0200

    updatefederation methods

--- restFederationHelper.groovy
+++ restFederationHelper.groovy
@@ -1,5 +1,6 @@
 package helpers
 
+import org.keycloak.admin.client.resource.ComponentResource
 import org.keycloak.admin.client.resource.RealmResource
 import org.keycloak.common.util.MultivaluedHashMap
 import org.keycloak.representations.idm.ComponentRepresentation
@@ -73,4 +74,53 @@ def add(final Map conf, RealmResource realmResource, log, comH, fedH, prop) {
             comH
     )
     fedH.triggerUpdate(component, realmResource, log, comH)
+}
+
+def updateFederation(final Map conf, RealmResource realmResource, log, comm) {
+    RealmRepresentation realm = realmResource.toRepresentation()
+
+    List<ComponentRepresentation> components = realmResource.components().query(realm.getId(),
+            "org.keycloak.storage.UserStorageProvider",
+            conf.name)
+    ComponentRepresentation component
+
+    if (components.size() == 1) {
+        ComponentRepresentation compPres = components.get(0)
+        compPres.config.with {
+            uppercase = ["true"]
+            fullSyncPeriod = [conf.fullSyncPeriod] //-1 for disabling
+            prefix = [conf.prefix]
+            cachePolicy = ["DEFAULT"]
+            priority = ["0"]
+            enabled = ["true"]
+            url = [conf.url]
+            uncheck_federation = [conf.uncheck_federation]
+            changedSyncPeriod = [conf.changedSyncPeriod] // -1 for disabling
+            role_sync = [conf.role_sync]
+            role_client_sync = [conf.role_client_sync]
+            attr_sync = [conf.attr_sync]
+            reset_action = [conf.reset_action]
+            evictionDay = []
+            evictionHour = []
+            evictionMinute = []
+            maxLifespan = []
+            proxy_enabled = [conf.proxy_enabled]
+            not_create_users = [conf.not_create_users]
+            by_pass = [conf.by_pass]
+        }
+
+        ComponentResource compRes = realmResource.components().component(compPres.getId())
+        compRes.update(compPres)
+
+        log.info(compPres.config.toMapString())
+        components = realmResource.components().query(realm.getId(),
+                "org.keycloak.storage.UserStorageProvider",
+                conf.name)
+        component = components.get(0)
+    } else {
+        component = components.get(0)
+        log.info("Component ${conf.name} could be not selected")
+    }
+
+    return component
 }
