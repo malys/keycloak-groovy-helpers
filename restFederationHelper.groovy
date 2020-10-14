552568837375ba850b028b6f3b2ec19cd8881756
Sylvain Malnuit <sylvain.malnuit@lyra-network.com>
2020-10-14 08:58:32 +0200

    add password sync parameter

--- restFederationHelper.groovy
+++ restFederationHelper.groovy
@@ -49,6 +49,10 @@ def createFederation(final Map conf, RealmResource realmResource, log, comm) {
             proxy_enabled = [conf.proxy_enabled]
             not_create_users = [conf.not_create_users]
             by_pass = [conf.by_pass]
+            password_sync = [conf.password_sync]
+            password_hash_algorithm = [conf.password_hash_algorithm]
+            password_hash_iteration[conf.password_hash_iteration]
+
         }
 
         log.info(compPres.config.toMapString())
@@ -109,6 +113,9 @@ def updateFederation(final Map conf, RealmResource realmResource, log, comm) {
             proxy_enabled = [conf.proxy_enabled]
             not_create_users = [conf.not_create_users]
             by_pass = [conf.by_pass]
+            password_sync = [conf.password_sync]
+            password_hash_algorithm = [conf.password_hash_algorithm]
+            password_hash_iteration[conf.password_hash_iteration]
         }
 
         ComponentResource compRes = realmResource.components().component(compPres.getId())
